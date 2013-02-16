a8b689df80abea8e377a4eaa69a0a0df0c02ae4a
Ryan Coppolo <r@coppolo.com>
2013-02-16 15:09:54 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweetbeetz::Application.config.secret_token = '0d55564f664632a97a9f5062d454bfb825f4cdd97196863216798511d578e735a540dc84f9689f6cf150cc36d02ff239d96c9b07d0d48d030bcacaea182b5399'

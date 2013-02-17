2fda29600e34cadf9f319d773c503e816358ba43
Ryan Fitzgerald <rwfitzge@gmail.com>
2013-02-17 15:09:44 -0800

    env var

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Tweetbeetz::Application.config.secret_token = '0d55564f664632a97a9f5062d454bfb825f4cdd97196863216798511d578e735a540dc84f9689f6cf150cc36d02ff239d96c9b07d0d48d030bcacaea182b5399'
+Tweetbeetz::Application.config.secret_token = ENV['SECRET_TOKEN'] || "91029381029830912830917230917230971230918230917230918230918230189273091273019273019273019273019273091273091237012937"

2d67f1c4f6f7837b8817219240ecf84cd1f5da05
Ryan Coppolo <r@coppolo.com>
2013-02-16 19:35:57 -0800

    Fixes routes for Devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Tweetbeetz::Application.routes.draw do
-
-  devise_scope :user do
-    delete "/logout" => "devise/sessions#destroy", as: :logout
-  end
-
-  match '/users/auth/twitter/callback' => 'callback#create', as: :auth
+  devise_for :users
 
   resources :beats
   root :to => 'beats#index'
 
+  match '/users/auth/twitter/callback' => 'callback#create', as: :auth
 
 end

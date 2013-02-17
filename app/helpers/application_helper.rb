5f9f98e46273101a3cfdadb25a68706d496b5266
Ryan Fitzgerald <rwfitzge@gmail.com>
2013-02-17 15:02:21 -0800

    Add credits to footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def credit_links
+    twitter_names = %w(rcoppola rrff__ fieldguider).shuffle
+
+    twitter_names.map do |name|
+      link_to "@#{name}", "https://twitter.com/#{name}"
+    end.to_sentence.html_safe
+  end
 end
